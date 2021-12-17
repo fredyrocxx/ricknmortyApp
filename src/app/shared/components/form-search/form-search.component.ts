431d73ab3153aef3a251516089ea9bfe2128fcbf
Fredrose Rebello <28035@telxsids.net>
2021-12-17 18:51:46 +0530

    commit

--- src/app/shared/components/form-search/form-search.component.ts
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form-search',
-  templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.scss']
+  template: `<input
+    #inputSearch
+    autofocus
+    type="text"
+    class="form-control-lg"
+    placeholder="Search...."
+    (keyup)="onSearch(inputSearch.value)"
+  />`,
+  styles: ['input{width:100%;}'],
 })
 export class FormSearchComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSearch(value: string) {
+    console.log('Buscar->',value)
+    //1000
+    if (value && value.length > 3) {
+      this.router.navigate(['/character-list'], { queryParams: { q: value } });
+    }
   }
-
 }
